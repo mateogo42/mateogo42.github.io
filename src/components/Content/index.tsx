d8d3de14a1bfa873d708347bde1c4bd40e99d3d9
Mateo Gonzalez <magonzalezoc@gmail.com>
2022-11-29 21:39:00 -0500

    Created basic sidebar and placeholder (or not?) content

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+const Content = function () {
+  return (
+    <div className="Content">
+      <h1>Welcome!!! :D</h1>
+      <p>
+        This is my personal website, I'm gonna use this to showcase some of my
+        projects and (hopefully) write some posts in the future.
+      </p>
+    </div>
+  );
+};
+export default Content;
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,20 @@
+import "./index.css";
+
+const Sidebar = function () {
+  return (
+    <div className="sidebar">
+      <div className="title">
+        <h1>mateogo42</h1>
+      </div>
+      <div className="nav">
+        <ul>
+          <li>home</li>
+          <li>projects</li>
+          <li>about me</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
